9dcb9874210896ca02a476e311956b774a7a108d
SwiatekOrg <31080298+SwiatekOrg@users.noreply.github.com>
2018-02-09 22:55:09 +0100

    UPDATE

--- test.py
+++ test.py
@@ -31,30 +31,30 @@ class MyKeyboard(Keyboard):
     def processKey(self, key):
         self.screen1.clear()
         if key == "2":
-            if self.y == 0:
-                self.screen1.pixel(self.x, self.y, True, redraw=True)
-                return None
+            if self.y == 0 and key == "2":
+                self.screen1.pixel(self.x, 7, True, redraw=True)
+                self.y = 8
             self.y = self.y - 1
             self.screen1.pixel(self.x,self.y,True, redraw=True)
             print("Kierunek gora")
         elif key == "4":
-            if self.x == 0:
-                self.screen1.pixel(self.x, self.y, True, redraw=True)
-                return None
+            if self.x == 0 and key == "4":
+                self.screen1.pixel( 7, self.y, True, redraw=True)
+                self.x = 8
             self.x = self.x - 1
             self.screen1.pixel(self.x,self.y,True, redraw=True)
             print("Kierunek lewo")
         elif key == "6":
-            if self.x == 7:
-                self.screen1.pixel(self.x, self.y, True, redraw=True)
-                return None
+            if self.x == 7 and key == "6":
+                self.screen1.pixel(0, self.y, True, redraw=True)
+                self.x = -1
             self.x = self.x + 1
             self.screen1.pixel(self.x,self.y,True, redraw=True)
             print("Kierunek prawo")
         elif key == "8":
-            if self.y == 7:
-                self.screen1.pixel(self.x, self.y, True, redraw=True)
-                return None
+            if self.y == 7 and key == "8":
+                self.screen1.pixel(self.x, 0, True, redraw=True)
+                self.y = -1
             self.y = self.y + 1
             self.screen1.pixel(self.x,self.y,True, redraw=True)
             print("Kierunek dol")
