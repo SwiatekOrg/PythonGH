f5b09d2841692c969b25ed19db8e5b7a487af930
SwiatekOrg <31080298+SwiatekOrg@users.noreply.github.com>
2018-02-17 00:08:47 +0100

    1

--- RPI/snake.py
+++ RPI/snake.py
@@ -7,33 +7,17 @@ import time
 
 screen = led.matrix(cascaded = 1)
 SIZE_LED = 8
-SIZE_SNAKE = 3
-x = 1
+x = 0
 y = 4
-
-screen.pixel(x, y, True, redraw=True)
-x+=1
-screen.pixel(x, y, True, redraw=True)
-x+=1
+licznik = 0
 screen.pixel(x, y, True, redraw=True)
 
+def DoPrzodu():
+    global x,y
+    time.sleep(0.4)
+    screen.pixel(x%SIZE_LED, y%SIZE_LED, False, redraw=True)
+    x += 1
+    screen.pixel(x%SIZE_LED, y%SIZE_LED, True, redraw=True)
 
 while True:
-    time.sleep(0.4)
-    screen.pixel((x+6)%SIZE_LED,y, False, redraw=True)
-    x += 1
-    screen.pixel(x%SIZE_LED, y, True, redraw=True)
-    if x == 5:
-        time.sleep(0.4)
-        screen.pixel((x + 6) % SIZE_LED, y, False, redraw=True)
-        y += 1
-        screen.pixel(x, y % SIZE_LED, True, redraw=True)
-        time.sleep(0.4)
-        screen.pixel((x + 7) % SIZE_LED, y-1, False, redraw=True)
-        y += 1
-        screen.pixel(x, y % SIZE_LED, True, redraw=True)
-        while True:
-            time.sleep(0.4)
-            screen.pixel(x, (y+6)%SIZE_LED, False, redraw=True)
-            y += 1
-            screen.pixel(x, y%SIZE_LED, True, redraw=True)
+    DoPrzodu()
