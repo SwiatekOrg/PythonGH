df794e3335ae3d824df6c36b64ecdbdacf64d23b
SwiatekOrg <31080298+SwiatekOrg@users.noreply.github.com>
2018-02-27 21:09:42 +0100

    1111

--- RPI/snake.py
+++ RPI/snake.py
@@ -5,93 +5,88 @@ from random import randint
 import random
 import time
 
-def __init__():
-    screen = led.matrix(cascaded = 1)
-    screen.clear()
+class Snake():
+    def __init__(self):
+        self.screen = led.matrix(cascaded = 1)
+        self.screen.clear()
 
-    SIZE_LED = 8
-    SNAKE_SIZE = 3
-    STARTX = 0
-    STARTY = 5
-    PRZERWA = 0.4
+        self.SIZE_LED = 8
+        self.SNAKE_SIZE = 3
+        self.STARTX = 0
+        self.STARTY = 5
+        self.PRZERWA = 0.4
+
+        self.x = self.STARTX
+        self.y = self.STARTY
+        self.pozycje = []
+        self.poprzedni = 0
 
 
-    dlugosc = SNAKE_SIZE
-    x = STARTX
-    y = STARTY
-    pozycje = []
-    poprzedni = 0
+    def Poczatek(self):
+        for i in range(0,self.SNAKE_SIZE):
+            self.Dodaj()
+            self.screen.pixel(self.x%self.SIZE_LED, self.y%self.SIZE_LED, True, redraw=True)
+            if i<self.SNAKE_SIZE-1:
+                self.x += 1
 
-def Poczatek():
-    global x
-    for i in range(0,SNAKE_SIZE):
-        Dodaj()
-        screen.pixel(x%SIZE_LED, y%SIZE_LED, True, redraw=True)
-        if i<SNAKE_SIZE-1:
-          x += 1
+    def Dodaj(self):
+        self.screen.pixel(self.x % self.SIZE_LED, self.y % self.SIZE_LED, True, redraw=True)
+        self.pozycje.append((str(self.x%self.SIZE_LED)+str(self.y%self.SIZE_LED)))
 
-def Dodaj():
-    screen.pixel(x % SIZE_LED, y % SIZE_LED, True, redraw=True)
-    pozycje.append((str(x%SIZE_LED)+str(y%SIZE_LED)))
+    def Usun(self):
+        self.screen.pixel((int(self.pozycje[0]) // 10),(int(self.pozycje[0]) % 10), False, redraw=True)
+        self.pozycje.remove(self.pozycje[0])
 
-def Usun():
-    screen.pixel((int(pozycje[0]) // 10),(int(pozycje[0]) % 10), False, redraw=True)
-    pozycje.remove(pozycje[0])
+    def DoPrzodu(self):
+        self.x += 1
 
-def DoPrzodu():
-    global x
-    x += 1
+    def DoTylu(self):
+        self.x -= 1
 
-def DoTylu():
-    global x
-    x -= 1
+    def WGore(self):
+        self.y -= 1
 
-def WGore():
-    global y
-    y -= 1
+    def WDol(self):
+        self.y += 1
 
-def WDol():
-    global y
-    y += 1
+    def LosowoCoKrok(self):
+        kierunek = randint(0,3)
+        if kierunek == 0:  #DoPrzodu
+            if self.poprzedni == 1:
+                self.DoTylu()
+                self.poprzedni = 1
+            else:
+                self.DoPrzodu()
+                self.poprzedni = 0
+        elif kierunek == 1: #DoTylu
+            if self.poprzedni == 0:
+                self.DoPrzodu()
+                self.poprzedni = 0
+            else:
+                self.DoTylu()
+                self.poprzedni = 1
+        elif kierunek == 2:  #WGore
+            if self.poprzedni == 3:
+                self.WDol()
+                self.poprzedni = 3
+            else:
+                self.WGore()
+                self.poprzedni = 2
+        elif kierunek == 3:   #WDol
+            if self.poprzedni == 2:
+                self.WGore()
+                self.poprzedni = 2
+            else:
+                self.WDol()
+                self.poprzedni = 3
 
-def LosowoCoKrok():
-    global poprzedni
-    kierunek = randint(0,3)
-    if kierunek == 0:  #DoPrzodu
-        if poprzedni == 1:
-            DoTylu()
-            poprzedni = 1
-        else:
-            DoPrzodu()
-            poprzedni = 0
-    elif kierunek == 1: #DoTylu
-        if poprzedni == 0:
-            DoPrzodu()
-            poprzedni = 0
-        else:
-            DoTylu()
-            poprzedni = 1
-    elif kierunek == 2:  #WGore
-        if poprzedni == 3:
-            WDol()
-            poprzedni = 3
-        else:
-            WGore()
-            poprzedni = 2
-    elif kierunek == 3:   #WDol
-        if poprzedni == 2:
-            WGore()
-            poprzedni = 2
-        else:
-            WDol()
-            poprzedni = 3
+    def SnakeLive(self):
+        self.Poczatek()
+        while True:
+            time.sleep(self.PRZERWA)
+            self.Usun()
+            self.LosowoCoKrok()
+            self.Dodaj()
 
-def SnakeLive():
-    Poczatek()
-    while True:
-        time.sleep(PRZERWA)
-        Usun()
-        LosowoCoKrok()
-        Dodaj()
-
-SnakeLive()
+snake = Snake()
+snake.SnakeLive()
