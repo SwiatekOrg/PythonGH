ccce542e481305603dc8b697a9ef02d54431fb9f
SwiatekOrg <31080298+SwiatekOrg@users.noreply.github.com>
2018-03-29 21:29:18 +0200

    1111

--- RPI/json/Zadanie2.py
+++ RPI/json/Zadanie2.py
@@ -1,6 +1,6 @@
 import json
 import requests
-import sys
+from datetime import datetime
 import time
 
 
@@ -30,6 +30,26 @@ for i in range(len(Czujniki)):
     print("")
     print(Krakow[i]['stationName'])
     for a in range(len(Czujniki[i])):
+
         czuj = requests.get('http://api.gios.gov.pl/pjp-api/rest/data/getData/'+str(Czujniki[i][a]['id']), headers)
         czuj_json = json.loads(czuj.content.decode('utf-8'))
-        print(str(czuj_json['key']) +" dla "+ str( czuj_json['values'][1]['date']) + " minut temu wartosc " + str(czuj_json['values'][1]['value']))
+
+        if czuj_json['values'][0]['value'] == None:
+            if czuj_json['values'][1]['value'] == None:
+                number = 2
+            else:
+                number = 1
+        else:
+            number = 0
+
+        czas = time.localtime()
+        dt_str = str(czuj_json['values'][number]['date'])
+        dt_obj = datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
+        czas = (str(str(czas[0]) + "-" + str(czas[1]) + "-" + str(czas[2]) + " " + str(czas[3]) + ":" + str(czas[4]) + ":" + str(czas[5])))
+        czas = datetime.strptime(czas, '%Y-%m-%d %H:%M:%S')
+        godzina1 = [dt_obj.hour, dt_obj.minute]
+        godzina2 = [czas.hour, czas.minute]
+        minuta = int((godzina2[0] - godzina1[0]) * 60 + (godzina2[1] - godzina1[1]))
+
+
+        print(str(czuj_json['key']) + " dla " + str(minuta) + " minut temu wartosc " + str(czuj_json['values'][number]['value']))
--- RPI/test.py
+++ RPI/test.py
@@ -1,4 +1,13 @@
+
+from datetime import datetime
 import time
 
 czas = time.localtime()
-print(str(czas[4]))
+dt_str = '9-24-2010 5:03:29'
+dt_obj = datetime.strptime(dt_str, '%m-%d-%Y %H:%M:%S')
+czas = (str(str(czas[0])+"-"+str(czas[1])+"-"+str(czas[2])+" "+str(czas[3])+":"+str(czas[4])+":"+str(czas[5])))
+czas = datetime.strptime(czas,'%Y-%m-%d %H:%M:%S')
+godzina1 = [dt_obj.hour,dt_obj.minute]
+godzina2 = [czas.hour,czas.minute]
+minuta = int( (godzina2[0]-godzina1[0])*60 + (godzina2[1]-godzina1[1]))
+print(minuta)
